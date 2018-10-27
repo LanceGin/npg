bfe017e0a0a667326680cdae4b8d148489454e7e
LanceGin <gin.lance.inside@hotmail.com>
2018-10-27 11:29:59 +0800

    add: prompt interaction

--- src/init.js
+++ src/init.js
@@ -1,21 +1,46 @@
 const path = require('path');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 const isDir = (name) => {
   const target = path.resolve(process.cwd(), path.join('.', name));
   try {
-    const isDir = fs.statSync(target).isDirectory();
-    if (isDir) {
-      return false;
-    } else {
-      return true;
-    }
+    const sign = fs.statSync(target).isDirectory();
+    return !sign && true;
   } catch (err) {
-    if (err.code === 'ENOENT') {
-      return true;
-    }
-  };
-}
+    return true;
+  }
+};
+
+const promptAns = (name) => {
+  const ans = inquirer.prompt([
+    {
+      name: 'name',
+      message: 'the package name:',
+      default: name,
+    },
+    {
+      name: 'version',
+      message: 'the package version:',
+      default: '0.0.1',
+    },
+    {
+      name: 'desc',
+      message: 'the description of the package:',
+      default: name,
+    },
+  ]);
+  return ans;
+};
+
+const createProject = (name) => {
+  console.log('create project.');
+  promptAns(name).then((ans) => {
+    console.log(`your package name is：${ans.name}`);
+    console.log(`your package version is：${ans.version}`);
+    console.log(`your package description is：${ans.desc}`);
+  });
+};
 
 const init = (program, name) => {
   // params name is required
@@ -25,10 +50,10 @@ const init = (program, name) => {
 
   // check the project name
   if (isDir(name)) {
-    console.log('create project.');
+    createProject(name);
   } else {
     console.log('project is already existed.');
   }
 };
- 
+
 export default init;
